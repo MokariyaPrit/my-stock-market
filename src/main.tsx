a49e1b338e7063325fac6e9129a7eb6fe2b4183a
mokariya prit <mokariyaprit1123@gmail.com>
2025-03-21 16:32:30 +0530

    2nd  commit

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { store } from "./store";
 import App from "./App";
 import "./index.css"
 import { AuthProvider } from "./context/AuthContext"; // Import AuthProvider
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
